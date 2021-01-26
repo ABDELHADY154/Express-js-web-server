4f65c9cf1f0f0f757b70f8df853c4e768384215a
YoussefElBeheiryy <77811664+YoussefElBeheiryy@users.noreply.github.com>
2021-01-26 03:14:43 +0200

    category view all

--- server.js
+++ server.js
@@ -8,75 +8,78 @@ var controller = require("./controller");
 app.set("view engine", "ejs");
 app.use(require("ejs-yield"));
 
-app.get("/", function (req, res) {
-  controller.LandingPageController(req, res);
+app.get("/", function(req, res) {
+    controller.LandingPageController(req, res);
 });
-app.get("/pharmacy", function (req, res) {
-  controller.HomeController(req, res);
+app.get("/pharmacy", function(req, res) {
+    controller.HomeController(req, res);
 });
-app.get("/login", function (req, res) {
-  controller.LoginController(req, res);
+app.get("/login", function(req, res) {
+    controller.LoginController(req, res);
 });
-app.get("/register", function (req, res) {
-  controller.RegisterController(req, res);
+app.get("/register", function(req, res) {
+    controller.RegisterController(req, res);
 });
-app.post("/registerUser", bodyParser, function (req, res) {
-  controller.RegisterFormController(req, res);
+app.post("/registerUser", bodyParser, function(req, res) {
+    controller.RegisterFormController(req, res);
 });
-app.post("/loginUser", bodyParser, function (req, res) {
-  controller.LoginFormController(req, res);
-});
-
-app.get("/logout", function (req, res) {
-  controller.logoutController(req, res);
+app.post("/loginUser", bodyParser, function(req, res) {
+    controller.LoginFormController(req, res);
 });
 
-app.get("/pharmacy/customers", function (req, res) {
-  controller.CustomersController(req, res);
-});
-app.get("/pharmacy/customers/show/:id", function (req, res) {
-  controller.CustomersShowController(req, res);
-});
-app.get("/pharmacy/customers/delete/:id", function (req, res) {
-  controller.CustomersDelete(req, res);
-});
-app.get("/pharmacy/customers/create", function (req, res) {
-  controller.createCustomer(req, res);
-});
-app.post("/pharmacy/customers/createForm", bodyParser, function (req, res) {
-  controller.createCustomerForm(req, res);
-});
-app.get("/pharmacy/customers/edit/:id", function (req, res) {
-  controller.editCustomer(req, res);
-});
-app.post("/pharmacy/customers/editForm/:id", bodyParser, function (req, res) {
-  controller.editCustomerForm(req, res);
+app.get("/logout", function(req, res) {
+    controller.logoutController(req, res);
 });
 
-app.get("/pharmacy/suppliers", function (req, res) {
-  controller.SupplierController(req, res);
+app.get("/pharmacy/customers", function(req, res) {
+    controller.CustomersController(req, res);
 });
-app.get("/pharmacy/suppliers/show/:id", function (req, res) {
-  controller.SupplierShowController(req, res);
+app.get("/pharmacy/customers/show/:id", function(req, res) {
+    controller.CustomersShowController(req, res);
 });
-app.get("/pharmacy/suppliers/delete/:id", function (req, res) {
-  controller.SupplierDelete(req, res);
+app.get("/pharmacy/customers/delete/:id", function(req, res) {
+    controller.CustomersDelete(req, res);
 });
-app.get("/pharmacy/suppliers/create", function (req, res) {
-  controller.CreateSupplier(req, res);
+app.get("/pharmacy/customers/create", function(req, res) {
+    controller.createCustomer(req, res);
 });
-app.post("/pharmacy/suppliers/createForm", bodyParser, function (req, res) {
-  controller.CreateSupplierForm(req, res);
+app.post("/pharmacy/customers/createForm", bodyParser, function(req, res) {
+    controller.createCustomerForm(req, res);
 });
-app.get("/pharmacy/suppliers/edit/:id", function (req, res) {
-  controller.editSupplier(req, res);
+app.get("/pharmacy/customers/edit/:id", function(req, res) {
+    controller.editCustomer(req, res);
 });
-app.post("/pharmacy/suppliers/editForm/:id", bodyParser, function (req, res) {
-  controller.editSupplierForm(req, res);
+app.post("/pharmacy/customers/editForm/:id", bodyParser, function(req, res) {
+    controller.editCustomerForm(req, res);
 });
 
+app.get("/pharmacy/suppliers", function(req, res) {
+    controller.SupplierController(req, res);
+});
+app.get("/pharmacy/suppliers/show/:id", function(req, res) {
+    controller.SupplierShowController(req, res);
+});
+app.get("/pharmacy/suppliers/delete/:id", function(req, res) {
+    controller.SupplierDelete(req, res);
+});
+app.get("/pharmacy/suppliers/create", function(req, res) {
+    controller.CreateSupplier(req, res);
+});
+app.post("/pharmacy/suppliers/createForm", bodyParser, function(req, res) {
+    controller.CreateSupplierForm(req, res);
+});
+app.get("/pharmacy/suppliers/edit/:id", function(req, res) {
+    controller.editSupplier(req, res);
+});
+app.post("/pharmacy/suppliers/editForm/:id", bodyParser, function(req, res) {
+    controller.editSupplierForm(req, res);
+});
+
+app.get("/pharmacy/category", function(req, res) {
+    controller.CategoryController(req, res);
+});
 // fallback route
-app.get("*", function (req, res) {
-  res.send("page not found");
+app.get("*", function(req, res) {
+    res.send("page not found");
 });
 app.listen(3000);
