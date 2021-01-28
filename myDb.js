8d26b204ad2b11f0bcd4a2fdae58705fcacdf852
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2021-01-28 23:32:28 +0200

    ..

--- myDb.js
+++ myDb.js
@@ -22,45 +22,55 @@ db.createDBIfNotExists = function (databaseName) {
     if (!err) console.log("database created");
   });
   con.query(
+    "CREATE DATABASE IF NOT EXISTS " + databaseName,
+    function (err, result) {
+      if (!err) console.log("database created");
+    },
+  );
+  con.query(
     "CREATE TABLE IF NOT EXISTS pharmacy.users (id INT AUTO_INCREMENT PRIMARY KEY , full_name VARCHAR(255), email VARCHAR(255), password VARCHAR(255))",
     function (err, result) {
       if (!err) console.log("users table created");
-    }
+    },
   );
   con.query(
     "CREATE TABLE IF NOT EXISTS pharmacy.customers (id INT AUTO_INCREMENT PRIMARY KEY , full_name VARCHAR(255), email VARCHAR(255), phone_num VARCHAR(15))",
     function (err, result) {
       if (!err) console.log("customers table created");
-    }
+    },
   );
   con.query(
     "CREATE TABLE IF NOT EXISTS pharmacy.suppliers (id INT AUTO_INCREMENT PRIMARY KEY , full_name VARCHAR(255), email VARCHAR(255), phone_num VARCHAR(15))",
     function (err, result) {
       if (!err) console.log("suppliers table created");
-    }
+    },
   );
-
   con.query(
-    "CREATE TABLE IF NOT EXISTS pharmacy.items (id INT AUTO_INCREMENT PRIMARY KEY ,item_name  VARCHAR(255), item_price  VARCHAR(255), item_quntitny VARCHAR(255)    )",
+    "CREATE TABLE IF NOT EXISTS pharmacy.category (id INT AUTO_INCREMENT PRIMARY KEY , name VARCHAR(255), description VARCHAR(255))",
     function (err, result) {
-      if (!err) console.log("items table created");
-    }
+      if (!err) console.log("category table created");
+    },
   );
-
 };
 
+// Authentication
 db.registerUser = function (data, userData) {
-  var q = "INSERT INTO pharmacy.users (full_name,email,password) VALUES (?,?,?)";
-  con.query(q, [data.full_name, data.email, data.password], function (err, result) {
-    if (!err) {
-      var q = "SELECT * FROM pharmacy.users WHERE email=? AND password=?";
-      con.query(q, [data.email, data.password], function (err, result) {
-        if (!err) {
-          userData(result[0]);
-        }
-      });
-    }
-  });
+  var q =
+    "INSERT INTO pharmacy.users (full_name,email,password) VALUES (?,?,?)";
+  con.query(
+    q,
+    [data.full_name, data.email, data.password],
+    function (err, result) {
+      if (!err) {
+        var q = "SELECT * FROM pharmacy.users WHERE email=? AND password=?";
+        con.query(q, [data.email, data.password], function (err, result) {
+          if (!err) {
+            userData(result[0]);
+          }
+        });
+      }
+    },
+  );
 };
 db.loginUser = function (data, user) {
   var q = "SELECT * FROM pharmacy.users WHERE email=? AND password=?";
@@ -71,6 +81,63 @@ db.loginUser = function (data, user) {
   });
 };
 
+// users
+db.getAllUsers = function (users) {
+  var q = "SELECT * FROM pharmacy.users WHERE 1";
+  con.query(q, function (err, result) {
+    if (!err) {
+      users(result);
+    }
+  });
+};
+
+db.getUser = function (id, getData) {
+  var q = "SELECT * FROM pharmacy.users WHERE id=?";
+  con.query(q, [id], function (err, result) {
+    if (!err) {
+      getData(result[0]);
+    }
+  });
+};
+
+db.deleteUser = function (id) {
+  var q = "DELETE FROM pharmacy.users WHERE id=?";
+  con.query(q, [id], function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
+
+db.createUser = function (data) {
+  var q =
+    "INSERT INTO pharmacy.users (full_name,email,password) VALUES (?,?,?)";
+  con.query(
+    q,
+    [data.full_name, data.email, data.password],
+    function (err, result) {
+      if (!err) {
+        console.log(result);
+      } else {
+        console.log(err);
+      }
+    },
+  );
+};
+db.updateUser = function (data, id) {
+  var q =
+    "UPDATE pharmacy.users SET id=?,full_name=?,email=?,password=? WHERE id=?";
+  con.query(
+    q,
+    [id, data.full_name, data.email, data.password, id],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
+};
+// customers
 db.getAllCustomer = function (customers) {
   var q = "SELECT * FROM pharmacy.customers WHERE 1";
   con.query(q, function (err, result) {
@@ -99,6 +166,7 @@ db.deleteCustomer = function (id) {
 };
 
 db.createCustomer = function (data) {
+<<<<<<< HEAD
   var q = "INSERT INTO pharmacy.customers (full_name,email,phone_num) VALUES (?,?,?)";
   con.query(q, [data.full_name, data.email, data.phone_num], function (err, result) {
     if (!err) {
@@ -110,6 +178,25 @@ db.createCustomer = function (data) {
 };
 db.updateCustomer = function (data, id) {
   var q = "UPDATE pharmacy.customers SET id=?,full_name=?,email=?,phone_num=? WHERE id=?";
+=======
+  var q =
+    "INSERT INTO pharmacy.customers (full_name,email,phone_num) VALUES (?,?,?)";
+  con.query(
+    q,
+    [data.full_name, data.email, data.phone_num],
+    function (err, result) {
+      if (!err) {
+        console.log(result);
+      } else {
+        console.log(err);
+      }
+    },
+  );
+};
+db.updateCustomer = function (data, id) {
+  var q =
+    "UPDATE pharmacy.customers SET id=?,full_name=?,email=?,phone_num=? WHERE id=?";
+>>>>>>> main
   con.query(
     q,
     [id, data.full_name, data.email, data.phone_num, id],
@@ -119,7 +206,11 @@ db.updateCustomer = function (data, id) {
       } else {
         console.log(err);
       }
+<<<<<<< HEAD
     }
+=======
+    },
+>>>>>>> main
   );
 };
 db.getAllSuppliers = function (customers) {
@@ -150,6 +241,7 @@ db.deleteSupplier = function (id) {
 };
 
 db.createSupplier = function (data) {
+<<<<<<< HEAD
   var q = "INSERT INTO pharmacy.suppliers (full_name,email,phone_num) VALUES (?,?,?)";
   con.query(q, [data.full_name, data.email, data.phone_num], function (err, result) {
     if (err) {
@@ -159,6 +251,23 @@ db.createSupplier = function (data) {
 };
 db.UpdateSupplier = function (data, id) {
   var q = "UPDATE pharmacy.suppliers SET id=?,full_name=?,email=?,phone_num=? WHERE id=?";
+=======
+  var q =
+    "INSERT INTO pharmacy.suppliers (full_name,email,phone_num) VALUES (?,?,?)";
+  con.query(
+    q,
+    [data.full_name, data.email, data.phone_num],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
+};
+db.UpdateSupplier = function (data, id) {
+  var q =
+    "UPDATE pharmacy.suppliers SET id=?,full_name=?,email=?,phone_num=? WHERE id=?";
+>>>>>>> main
   con.query(
     q,
     [id, data.full_name, data.email, data.phone_num, id],
@@ -166,6 +275,7 @@ db.UpdateSupplier = function (data, id) {
       if (err) {
         console.log(err);
       }
+<<<<<<< HEAD
     }
   );
 };
@@ -220,9 +330,59 @@ db.deleteItems = function (id) {
   var q = "DELETE FROM pharmacy.items WHERE id=?";
   con.query(q, [id], function (err, result) {
     if (err) {
-      console.log(err);
+=======
+    },
+  );
+};
+
+db.getAllCategory = function (category) {
+  var q = "SELECT * FROM pharmacy.category WHERE 1";
+  con.query(q, function (err, result) {
+    if (!err) {
+      category(result);
+    }
+  });
+};
+db.getCategory = function (id, getData) {
+  var q = "SELECT * FROM pharmacy.category  WHERE id=?";
+  con.query(q, [id], function (err, result) {
+    if (!err) {
+      getData(result[0]);
+    }
+  });
+};
+db.deleteCategory = function (id) {
+  var q = "DELETE FROM pharmacy.category WHERE id=?";
+  con.query(q, [id], function (err, result) {
+    if (!err) {
+      console.log(result);
     }
   });
 };
 
+db.createCategory = function (data) {
+  var q = "INSERT INTO pharmacy.category (name,description) VALUES (?,?)";
+  con.query(q, [data.name, data.description], function (err, result) {
+    if (!err) {
+      console.log(result);
+    } else {
+      console.log(err);
+    }
+  });
+};
+db.updateCategory = function (data, id) {
+  var q = "UPDATE pharmacy.category SET id=?,name=?,description=? WHERE id=?";
+  con.query(q, [id, data.name, data.description, id], function (err, result) {
+    if (!err) {
+      console.log(result);
+    } else {
+>>>>>>> main
+      console.log(err);
+    }
+  });
+};
+<<<<<<< HEAD
+
+=======
+>>>>>>> main
 module.exports = db;
--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get("/", function (req, res) {
 app.get("/pharmacy", function (req, res) {
   controller.HomeController(req, res);
 });
+<<<<<<< HEAD
+=======
+// Authentication
+>>>>>>> main
 app.get("/login", function (req, res) {
   controller.LoginController(req, res);
 });
@@ -25,12 +29,44 @@ app.post("/registerUser", bodyParser, function (req, res) {
 });
 app.post("/loginUser", bodyParser, function (req, res) {
   controller.LoginFormController(req, res);
+<<<<<<< HEAD
 });
 
 app.get("/logout", function (req, res) {
   controller.logoutController(req, res);
 });
 
+=======
+});
+app.get("/logout", function (req, res) {
+  controller.logoutController(req, res);
+});
+
+//Users
+app.get("/pharmacy/users", function (req, res) {
+  controller.UsersController(req, res);
+});
+app.get("/pharmacy/users/show/:id", function (req, res) {
+  controller.UsersShowController(req, res);
+});
+app.get("/pharmacy/users/delete/:id", function (req, res) {
+  controller.UserDelete(req, res);
+});
+app.get("/pharmacy/users/create", function (req, res) {
+  controller.createUser(req, res);
+});
+app.post("/pharmacy/users/createForm", bodyParser, function (req, res) {
+  controller.createUserForm(req, res);
+});
+app.get("/pharmacy/users/edit/:id", function (req, res) {
+  controller.editUser(req, res);
+});
+app.post("/pharmacy/users/editForm/:id", bodyParser, function (req, res) {
+  controller.editUserForm(req, res);
+});
+
+// Customers
+>>>>>>> main
 app.get("/pharmacy/customers", function (req, res) {
   controller.CustomersController(req, res);
 });
@@ -53,6 +89,10 @@ app.post("/pharmacy/customers/editForm/:id", bodyParser, function (req, res) {
   controller.editCustomerForm(req, res);
 });
 
+<<<<<<< HEAD
+=======
+// suppliers
+>>>>>>> main
 app.get("/pharmacy/suppliers", function (req, res) {
   controller.SupplierController(req, res);
 });
@@ -75,8 +115,32 @@ app.post("/pharmacy/suppliers/editForm/:id", bodyParser, function (req, res) {
   controller.editSupplierForm(req, res);
 });
 
+<<<<<<< HEAD
 app.get("/pharmacy/items", function (req, res) {
   controller.ItemController(req, res);
+=======
+// category
+app.get("/pharmacy/category", function (req, res) {
+  controller.CategoryController(req, res);
+});
+app.get("/pharmacy/category/show/:id", function (req, res) {
+  controller.CategoryShowController(req, res);
+});
+app.get("/pharmacy/category/delete/:id", function (req, res) {
+  controller.CategoryDelete(req, res);
+});
+app.get("/pharmacy/category/create", function (req, res) {
+  controller.CreateCategory(req, res);
+});
+app.post("/pharmacy/category/createForm", bodyParser, function (req, res) {
+  controller.CreateCategoryForm(req, res);
+});
+app.get("/pharmacy/category/edit/:id", function (req, res) {
+  controller.editCategory(req, res);
+});
+app.post("/pharmacy/category/editForm/:id", bodyParser, function (req, res) {
+  controller.updateCategoryForm(req, res);
+>>>>>>> main
 });
 app.get("/pharmacy/items/show/:id", function (req, res) {
   controller.ItemShowController(req, res);
@@ -103,4 +167,8 @@ app.get("/pharmacy/items/delete/:id", function (req, res) {
 app.get("*", function (req, res) {
   res.send("page not found");
 });
+<<<<<<< HEAD
+=======
+
+>>>>>>> main
 app.listen(3000);
